3de7fb210a7402b4994530db1baccbfd20e2d23e
Christian Kniep <ckniep@gaikai.com>
2015-08-28 11:41:47 +0200

    enable skip of services via ENV

--- opt/qnib/bin/supervisor_daemonize.sh
+++ opt/qnib/bin/supervisor_daemonize.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
 sed -i -e 's/nodaemon=.*/nodaemon=false/' /etc/supervisord.conf 
+
+## DISABLE services if demanded (via comma separated list in SUPERVISOR_SKIP_SRV)
+for srv in $(echo ${SUPERVISOR_SKIP_SRV}|tr "," " ");do
+    if [ -f /etc/supervisord.d/${srv}.ini ];then
+        if grep autostart /etc/supervisord.d/${srv}.ini;then
+            sed -i'' -e 's/^autostart.*/autostart=false/' /etc/supervisord.d/${srv}.ini
+        else
+            sed -i'' '/^command/a autostart=false' /etc/supervisord.d/${srv}.ini
+        fi
+    else
+        echo "ERR> could not find service '${srv}' (/etc/supervisord.d/${srv}.ini) to disable it"
+    fi
+done
+
 supervisord -c /etc/supervisord.conf 
 sleep 5
 echo "# supervisorctl status"
