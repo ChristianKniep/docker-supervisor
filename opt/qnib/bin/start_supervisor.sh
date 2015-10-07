d0621e51eb8208e4c929a7640cc43642cdfd9773
Christian Kniep <christian@qnib.org>
2015-10-07 13:54:30 +0200

    add SUPERVISOR_START_EXTERNAL

--- opt/qnib/bin/start_supervisor.sh
+++ opt/qnib/bin/start_supervisor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sed -i -e 's/nodaemon=.*/nodaemon=false/' /etc/supervisord.conf 
+sed -i -e 's/nodaemon=.*/nodaemon=false/' /etc/supervisord.conf
 
 ## DISABLE services if demanded (via comma separated list in SUPERVISOR_SKIP_SRV)
 for srv in $(echo ${SUPERVISOR_SKIP_SRV}|tr "," " ");do
@@ -14,6 +14,15 @@ for srv in $(echo ${SUPERVISOR_SKIP_SRV}|tr "," " ");do
         echo "ERR> could not find service '${srv}' (/etc/supervisord.d/${srv}.ini) to disable it"
     fi
 done
+
+## Fire up script outside of supervisor
+if [ "X${SUPERVISOR_START_EXTERNAL}" != "X" ];then
+    if [ -x ${SUPERVISOR_START_EXTERNAL} ];then
+        ${SUPERVISOR_START_EXTERNAL} &
+    else
+        echo "Sorry, '${SUPERVISOR_START_EXTERNAL}' has to ba a file and executable..."
+    fi
+fi
 
 if [ "X$1" == "X" ];then
     supervisord -c /etc/supervisord.conf
